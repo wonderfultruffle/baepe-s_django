50be28dd1d50f5fb5f6aafb23501559a1e28339a
jh <jh2two@gmail.com>
2023-09-08 22:50:48 +0900

    baepue_6.5.3: add 'product_in_category' and 'product_in_detail' views

--- onlineshop/shop/views.py
+++ onlineshop/shop/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
 
 # Create your views here.
+def products_in_category(request, category_slug=None):
+    current_category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available_display=True)
+
+    if category_slug:
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=current_category)
+
+    return render(request, "shot/list.html", {"current_category":current_category, "categories":categories, "products": products})
+
+def product_in_detail(request, id, product_slug=None):
+    product = get_object_or_404(Product, id, slug=product_slug)
+
+    return render(request, "shop/detail.html", {"product":product})
