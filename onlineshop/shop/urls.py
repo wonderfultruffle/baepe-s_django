8a36c121d67e127924750ebae3aa21f7109f44ca
jh <jh2two@gmail.com>
2023-09-08 23:55:16 +0900

    baepeu_6.5.5: add templates(base, list and detail)

--- onlineshop/shop/urls.py
+++ onlineshop/shop/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import products_in_category, product_in_detail
+from .views import product_in_category, product_in_detail
 
 urlpatterns = [
-    path("", products_in_category, name="product_all"),
-    path("<slug:category_slug>", products_in_category, name="products_in_category"),
-    path("<id:id><slug:product_slug>", product_in_detail, name="product_detail"),
+    path("", product_in_category, name="products_all"),
+    path("<slug:category_slug>", product_in_category, name="products_in_category"),
+    path("<int:id><slug:product_slug>", product_in_detail, name="product_detail"),
 ]
--- onlineshop/shop/views.py
+++ onlineshop/shop/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 
 # Create your views here.
-def products_in_category(request, category_slug=None):
+def product_in_category(request, category_slug=None):
     current_category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available_display=True)
@@ -11,7 +11,7 @@ def products_in_category(request, category_slug=None):
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=current_category)
 
-    return render(request, "shot/list.html", {"current_category":current_category, "categories":categories, "products": products})
+    return render(request, "shop/list.html", {"current_category":current_category, "categories":categories, "products": products})
 
 def product_in_detail(request, id, product_slug=None):
     product = get_object_or_404(Product, id, slug=product_slug)
